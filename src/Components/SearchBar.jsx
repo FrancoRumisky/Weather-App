0fd29a87cfb63689384b29803b991c41072e49a9
FrancoRumiky <framqoo@gmail.com>
2022-12-28 20:47:44 -0300

    fix bug: searchbar now navigate to / when search citi

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import style from "./styles/SearchBar.module.css"
 
 export default function SearchBar({onSearch}) {
   let [inputValue,setInputValue] = useState("");
-  // acá va tu código
+
+const history = useHistory()
+
+  const search = () => {
+    onSearch(inputValue)
+    setInputValue("")
+    history.push("/")
+  }
+  
   return <div>
             <input className={style.citi} type="text" placeholder="Cuidad..." value={inputValue}
             onChange={(e)=>setInputValue(e.target.value)} >
             </input>
-            <button className={style.btn} onClick={(e) => {onSearch(inputValue); setInputValue("")}} > Agregar 
+            <button className={style.btn} onClick={(e) => search()}> Agregar 
             </button>  
           </div>
          
